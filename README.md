6dcdd185d0542ef93b2b4eca57539b180101fe94
yatharth <yatharth.saini@paymeindia.in>
2024-06-08 16:42:20 +0530

    UPDATED: the intial commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # CrisilScraper
+
 Data scraping from the Crisil site as part of an assessment
 
-# creating a virtual env :(installing playwirght package in the requirements)
+1. Creating a virtual env for installing the requirements : in this case it is just the playwright package of python
+2. pip install playwright # terminal command 
+3. now hitting the pip freeze > requirements.txt to store the installed dependencies on a text file for code
+maintenance
+4. hit the _command_ : playwright install in your terminal (Will take 4-5 minutes .... 
+
+TO run the script for the crisil scraping 
+1. run the main.py script in your terminal 
+2. it will prompt you to enter an input str: that will be the company name
+3. type the desired company name and press Enter
+
+
+Monitor the logs..........
--- main.py
+++ main.py
@@ -14,31 +14,123 @@ issues.
 from playwright.sync_api import sync_playwright
 from typing import Any
 import logging
-
+import re
 logger = logging.getLogger(__name__)
 
 URL = "https://www.crisilratings.com/en/home/our-business/ratings.html"
 
 
-def get_crisil_info(company_name: str) -> dict[str, Any]:
-    """
-    :param company_name: The name of the company to search for.
+class CrisilGet:
 
-    :return: A dictionary containing the following information about the company:
-        - Company Name
-        - Company Rating
-        - Company Rating Type
-        - Company Rating Date
-        - Company Rating Outlook
-        - Company Rating Rationale URL : URL to the Crisil website where the rating rationale can be found.
+    def extract_date_from_outlook(self, outlook_text):
+        """
+        regex function helper to extract the date from the outlook value
+        """
+        if outlook_text:
+            # Use regular expression to find date string
+            match = re.search(
+                r'(\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}\b)',
+                outlook_text)
+            if match:
+                return match.group(0)
+        return None
 
-    If the company is not found, return an empty dictionary.
-    """
-    logger.info(f"Getting Crisil info for {company_name}")
-    # TODO: Implement me
-    # Use Python Playwright to scrape data from the Crisil website.
+    def get_crisil_info(self, company_name: str) -> dict[str, Any]:
+        """
+        :param company_name: The name of the company to search for.
 
-    return {}
+        :return: A dictionary containing the following information about the company:
+            - Company Name
+            - Company Rating
+            - Company Rating Type
+            - Company Rating Date
+            - Company Rating Outlook
+            - Company Rating Rationale URL : URL to the Crisil website where the rating rationale can be found.
+
+        If the company is not found, return an empty dictionary.
+        """
+        with sync_playwright() as play_instance:
+            playground = play_instance.chromium.launch(headless=False, slow_mo=300)
+            web_page = playground.new_page()
+            web_page.goto(URL)
+            # searching the search box
+            web_page.locator("input.sme_rating_search_input").fill(company_name)
+            web_page.wait_for_timeout(1000)
+
+            # checking if any suggestions land up
+            suggestions = web_page.locator('.ui-menu-item')
+            if suggestions.count() > 0:
+                # hitting the first suggestion in case of a suggestions list
+                suggestions.first.click()
+                logger.info(f"Found some suggestions relating to {company_name}")
+                logger.info(f"Hitting the closest suggestion to the {company_name}")
+            else:
+                # if no suggestions then hitting enter just
+                web_page.keyboard.press("Enter")
+
+            try:
+                # trying to locate Found 0 results : in case of found we will terminate the scraping further
+                # this will take time in case of no results locator is not found
+
+                no_results_found = web_page.locator('.sme-search-results-found')
+                if no_results_found and no_results_found.inner_text().__contains__("Found 0 results"):
+                    logger.error("Could not find any crisil results for the company name")
+                    playground.close()
+                    return {}
+            except:
+                pass
+
+            # locating the company name
+            company_name_locator = web_page.locator("h3.crisil-sub-heading")
+            company_name = company_name_locator.text_content()
+            result_dict = {'company_name': company_name}
+
+            # getting the ratings item list
+            items = web_page.query_selector_all('.comp-fs-instrument-container .item')
+
+            crisil_scraped_data = []
+            for item in items:
+                # iterate over the items
+                # instrument category
+                instrument_element = item.query_selector('li:has(span:text("Instrument Category")) h4')
+                instrument_category_text = instrument_element.inner_text() if instrument_element else None
+
+                # rating category
+                ratings_element = item.query_selector('li:has(span:text("Ratings")) h4')
+                crisil_ratings_text = ratings_element.inner_text() if ratings_element else None
+
+                # outlook category
+                outlook_element = item.query_selector('li:has(span:text("Outlook")) h4')
+                outlook_text = outlook_element.inner_text() if outlook_element else None
+
+                # scrapping the outlook date from the outlook using regex
+                outlook_date = self.extract_date_from_outlook(outlook_text)
+
+                iteration_dict = {
+                    'instrument': instrument_category_text,
+                    'rating': crisil_ratings_text,
+                    'outlook': outlook_text,
+                    'outlook date': outlook_date
+                }
+                crisil_scraped_data.append(iteration_dict)
+
+            result_dict |= {'ratings': crisil_scraped_data}
+
+            # Click the button
+            web_page.click('a.rr-doc-class')
+
+            # Wait for navigation to complete
+            web_page.wait_for_load_state('networkidle', timeout=30000)
+
+            # Get the URL after clicking the button
+            url_after_click = web_page.url
+            result_dict |= {'rationale_url': url_after_click}
+
+            # closing the web browser
+            playground.close()
+
+        logger.info(f"Extracted data for {company_name} is : {result_dict}")
+        return {"Comapny Name": company_name, "Crisil Data": result_dict}
 
 
 def main():
@@ -60,7 +152,14 @@ def main():
     "rating_date": "September 11, 2023", # Date can be any format
     }
     """
-    get_crisil_info(company_name="Reliance Jio Infocomm")
+    crisil_get = CrisilGet()
+    company_name = input(str)
+
+    logger.info(f"Getting Crisil info for {company_name}")
+    res_dict = crisil_get.get_crisil_info(company_name=company_name)
+    print("Scraped data we got is: ", res_dict)
+    logger.info(f"Crisil scraping completed for {company_name}")
+    print("")
 
 
 if __name__ == "__main__":
